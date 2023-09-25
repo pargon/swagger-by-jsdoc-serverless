afeb82446bc0a780836dec1e732f7ab109aa2e61
gnparra <gonza.parra@gmail.com>
2023-09-25 16:29:47 -0300

    copy from jsdoc with body

---
+++ bin/indexDev.js
@@ -0,0 +1,7 @@
+/* eslint-disable class-methods-use-this, no-console, max-len */
+const spec = require("../lib/specJson");
+// const dHtml = require("../lib/jsonHtml");
+
+
+spec.eject();
+console.log("index Dev");
